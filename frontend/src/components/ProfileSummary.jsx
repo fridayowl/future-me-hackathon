278800796d4fcd4041a501446a8c349e06d79ed0
abhitrueprogrammer <abhiaruvs3@gmail.com>
2025-07-26 20:20:59 +0530

    layout

--- frontend/src/components/ProfileSummary.jsx
+++ frontend/src/components/ProfileSummary.jsx
@@ -3,42 +3,62 @@
 
 const ProfileSummary = ({ humanDescription }) => {
   if (!humanDescription) return null;
-  const { name, age, lifeStage, netWorth, totalLiability, creditScore, employment } = humanDescription;
+  const {
+    name,
+    age,
+    lifeStage,
+    netWorth,
+    totalLiability,
+    creditScore,
+    employment,
+  } = humanDescription;
   return (
     <div className="profile-analysis" style={{ backgroundColor: "#0b0f10" }}>
       <div className="preview-section">
         <div className="preview-grid preview-item">
           <div className="preview-item">
             <span className="text-4xl preview-value">{name}</span>
-            <div className="text-xs" >{lifeStage}</div>
+            <div className="text-xs">{lifeStage}</div>
           </div>
           <div className=" preview-item ">
-            <span className="preview-value text-5xl text-center items-center flex justify-center">{age}</span>
+            <span className="preview-value text-5xl text-center items-center flex justify-center">
+              {age}
+            </span>
           </div>
 
           <div className="preview-item">
             <span className="preview-label">Net Worth</span>
-            <span className="preview-value">₹{parseInt(netWorth).toLocaleString()}</span>
+            <span className="preview-value">
+              ₹{parseInt(netWorth).toLocaleString()}
+            </span>
           </div>
           <div className="preview-item">
             <span className="preview-label">Total Liability</span>
-            <span className="preview-value">₹{parseInt(totalLiability).toLocaleString()}</span>
+            <span className="preview-value">
+              ₹{parseInt(totalLiability).toLocaleString()}
+            </span>
           </div>
-          <div className="preview-item twoXL">
+          <div className="preview-item twoXL text-center">
             <span className="preview-label">Credit Score</span>
             <span className="preview-value">{creditScore}</span>
           </div>
-          <div className="preview-item">
-            <span className="preview-label">Employer</span>
-            <span className="preview-value">{employment?.currentEmployer}</span>
-          </div>
-          <div className="preview-item">
-            <span className="preview-label">Work Experience (years)</span>
-            <span className="preview-value">{employment?.workExperienceYears}</span>
-          </div>
-          <div className="preview-item">
-            <span className="preview-label">Career Trajectory</span>
-            <span className="preview-value">{employment?.careerTrajectory}</span>
+          <div className="preview-item text-center twoXL">
+            <div>
+              <span className="preview-label">Employer</span>
+              <span className="preview-value">
+                {employment?.currentEmployer}
+              </span>
+            </div>
+            <div>
+              <span className="preview-label">Work Experience: </span>
+              <span className="preview-value">
+                {employment?.workExperienceYears}yrs | 
+                <span className="preview-label"> Career Trajectory: </span>
+                <span className="preview-value">
+                  {employment?.careerTrajectory}
+                </span>
+              </span>
+            </div>
           </div>
         </div>
       </div>
@@ -47,4 +67,3 @@ const ProfileSummary = ({ humanDescription }) => {
 };
 
 export default ProfileSummary;
-  
